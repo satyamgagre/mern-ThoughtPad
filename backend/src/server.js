48dc35be242d99c4e3ac03f6795d7872e3bed3cb
Satyam Gagare <satyamgagre4@gmail.com>
2025-12-25 02:26:07 +0530

    Prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,17 +5,22 @@ import cors from "cors";
 import notesRoutes from "./routes/notesroutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path";
+
 
 
 console.log("UPSTASH URL in server:", process.env.UPSTASH_REDIS_REST_URL);
 console.log("MONGO_URI =", process.env.MONGO_URI);
 
 const app = express();
+const __dirname = path.resolve();
 
 // Middlewares
+if (process.env.NODE_ENV !== "production") {
 app.use(cors({
   origin: "http://localhost:5173", // Adjust as needed for your frontend
 }));
+}
 
 app.use(express.json());
 app.use(rateLimiter);
@@ -28,6 +33,12 @@ app.use((req, res, next) => {
 // Routes
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 // Connect DB and start server
 const PORT = process.env.PORT || 5001;
 
